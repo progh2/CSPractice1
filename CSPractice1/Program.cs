c467fc4c0785ec28a6fa5e7ca955cb09796ee02b
Gihun Ham <ham@e-mirim.hs.kr>
2024-06-28 09:47:55 +0900

    #1 12-9 입력

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -82,6 +82,10 @@ namespace CSPractice1
             var varnumber = 20;
             // var abc;  // x. 반드시 초기화 해줘야 함
 
+            // #1 12-9 입력
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+
 
 
         }
