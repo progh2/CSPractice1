a9d7c66a6efc5792b438a24f555e32708091abe9
Gihun Ham <ham@e-mirim.hs.kr>
2024-06-28 09:57:14 +0900

    #2 13-6. 삼항연산자

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -116,6 +116,9 @@ namespace CSPractice1
                     break;
             }
 
+            // #2 13-6. 삼항연산자
+            Console.WriteLine(number %2 != 1 ? "짝수" : "홀수");
+
 
         }
     }
