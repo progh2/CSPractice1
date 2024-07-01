b71d8e3a4c3f8b37f5d2d68b3a880f62c721c1ce
Gihun Ham <ham@e-mirim.hs.kr>
2024-07-01 13:40:30 +0900

    #5 22-3. 클래스 메서드

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -7,6 +7,18 @@ using System.Threading.Tasks;
 
 namespace CSPractice1
 {
+    class MyMath
+    {
+        public static int classVar = 1;
+        public int instanceVar = 2;
+        public static int Abs(int input)
+        {
+            Console.WriteLine(classVar);
+            // 클래스 메서드에서 인스턴스 변수 접근 불가
+            // Console.WriteLine(instanceVar); 
+            return (input >= 0) ? input : -input;
+        }
+    }
 
     class FirstClass
     {
@@ -187,6 +199,7 @@ namespace CSPractice1
             Console.WriteLine(Product.TAX_RATIO);
             // Console.WriteLine(p1.TAX_RATIO);  // Java는 가능하나 C#은 X
 
+            // #5 22-3. 클래스 메서드
 
         }
     }
